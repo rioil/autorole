9f905f3e393bdac7291c4c72fb127cb2a670caf4
rioil <rioil.dev@gmail.com>
2023-12-09 17:55:41 +0900

    fix build script

--- package.ps1
+++ package.ps1
@@ -5,7 +5,7 @@ Copy-Item -Path ./manifest.json -Destination ./dist/ -Force
 
 # Compress files to xpi
 New-Item -Path ./output/ -Type Directory -Force
-Compress-Archive -Path ./dist/ -DestinationPath ./output/autorole.zip -Update
+Compress-Archive -Path ./dist/* -DestinationPath ./output/autorole.zip -Update
 Move-Item -Path ./output/autorole.zip -Destination ./output/autorole.xpi -Force
 
 # Create source code zip
